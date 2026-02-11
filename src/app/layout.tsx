18fbaad3cd74d5287b4f57011ec31fa8cada5782
Eduardo Santos Bezerra <eduardosantosbezz@gmail.com>
2026-02-11 21:24:12 +0000

    o favicon nao está indo o novo. veja se está certo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   title: 'A Anomia',
   description: 'Um blog contemporâneo sobre filosofia.',
   icons: {
-    icon: '/favicon.png',
+    icon: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚪️</text></svg>",
   }
 };
 
